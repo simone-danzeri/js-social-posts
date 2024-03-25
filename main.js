925a100337fde9193d999b03d312ca05d12bf2e8
simone-danzeri <s.danzeri@icloud.com>
2024-03-25 13:53:08 +0100

    feat: milestone 1 completa

--- main.js
+++ main.js
@@ -74,37 +74,46 @@ const postContainer = document.querySelector(".posts-list");
 console.log(postContainer);
 // Scorro gli elementi dell'array post e stampo in pagina ogni elemento
 posts.forEach((eachPost) => {
-  console.log(eachPost);
-  postContainer.innerHTML += `
+  const postTemplate = generateSinglePost(eachPost);
+  postContainer.innerHTML += postTemplate;
+});
+
+// FUNCTIONS
+function generateSinglePost(eachPost) {
+  // Estrapolo i dati dagli oggetti
+  const { id, media, author, likes, created } = eachPost;
+  // Stampo in pagina ogni post
+  const postTemplate = `
     <div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                    <img class="profile-pic" src="${author.image}" alt="Author">                    
                 </div>
                 <div class="post-meta__data">
-                    <div class="post-meta__author">Phil Mangione</div>
-                    <div class="post-meta__time">4 mesi fa</div>
+                    <div class="post-meta__author">${author.name}</div>
+                    <div class="post-meta__time">${created}</div>
                 </div>                    
             </div>
         </div>
         <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
         <div class="post__image">
-            <img src="https://unsplash.it/600/300?image=171" alt="">
+            <img src="${media}" alt="">
         </div>
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
                 </div>
             </div> 
         </div>            
     </div>
     `;
-});
+  return postTemplate;
+}
